819b9b0626660e6956db2bd19fb75cd8f9a4991e
gabriel izebhigie <gabrielizebhigie@isw-gstad-oj.interswitchng.com>
2018-01-15 14:46:57 +0100

    Initial Commit

---
+++ objcdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objcdemo
+//
+//  Created by gabriel izebhigie on 15/01/2018.
+//  Copyright © 2018 Interswitch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
